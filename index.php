06e5a969f047aef4bfcb0f5bd5aaf638640c0c3e
19Hamid91 <abdurrahmanhamid91@gmail.com>
2022-06-25 12:01:18 +0700

    tambah list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam illum quasi error animi, nisi quisquam quaerat fugiat ducimus. Numquam voluptas sed alias dolore adipisci! Delectus modi eum ducimus cupiditate expedita.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
